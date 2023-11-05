a6c19be03422e08c7d178610b25ec810eaee1149
Bruno Sarmento <120613201+BrunoSarmento-dev@users.noreply.github.com>
2023-11-05 08:01:42 -0300

    FakePinterest - Base

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def homepage():
-    return "FakePinterest"
-
+from fakepinterest import app
 
 if __name__ == '__main__':
     app.run(debug=True)
